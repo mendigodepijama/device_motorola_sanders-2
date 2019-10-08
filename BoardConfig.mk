86edcdbba8f66fb6ebc76c125eee66604b009d6b
lucasponez <lucasponez@outlook.com>
2019-10-08 21:20:18 +0000

    sanders: Include sepolicy
    Change-Id: I41d6557dc5866e50c45c07780b1b628d50b6e1b7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,8 +226,9 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2018-11-5
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
